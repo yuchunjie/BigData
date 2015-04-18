21d4cee5740ca9017d2be78141aed1eede8049f2
chunjie.yu <chunjie.yu@msa.hinet.net>
2015-04-18 18:12:20 +0800

    [logstash] Parsing source IP form spam_mail

--- HomeWork_1/Source_Code/Logstash/wr_spam_mail_2_el.sh
+++ HomeWork_1/Source_Code/Logstash/wr_spam_mail_2_el.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 logstash_log_base=~/logstash-1.4.2/_sample_log/spam_mail_log/
+logstash_conf=wr_spam_mail_2_el.conf
 curl -XDELETE 'hadoop01:9200/sample'
 rm $logstash_log_base/sincedb.db
-./logstash -f wr_spam_mail_2_el.conf | tee wr_spam_mail_2_el.log
+./logstash -f $logstash_conf  --configtest &&  ./logstash -f $logstash_conf | tee wr_spam_mail_2_el.log
